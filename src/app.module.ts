66e8bdf1342554606eb5f570659639850acf9090
Doha M Tubaileh <doha.tubaileh@gmail.com>
2026-07-22 20:53:23 +0300

    update import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from './database/database.module';
-import { ExpenseModule } from './expense/expense.module';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './services/database/database.module';
+import { ExpenseModule } from './services/expense/expense.module';
+import { AuthModule } from './services/auth/auth.module';
+import { UsersModule } from './services/users/users.module';
 
 @Module({
   imports: [DatabaseModule, ExpenseModule, AuthModule, UsersModule],
